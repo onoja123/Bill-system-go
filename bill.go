d340b8db289d92e1f03ddd3ec806f139c7f0edda
okpe onoja <okpeonoja18@gmail.com>
2023-03-07 02:11:39 +0100

    first push

---
+++ bill.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+
+	var total float64 = 0
+
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v...$%v \n", k+": ", v)
+
+		total += v
+	}
+
+	fs += fmt.Sprintf("%-25v ....$%0.2f", "total :", total)
+
+	return fs
+}
