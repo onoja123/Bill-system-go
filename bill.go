41695a4cac1c18e2986b5c6fc833bc8b5789008e
okpe onoja <okpeonoja18@gmail.com>
2023-03-10 00:35:29 +0100

    added user input

--- bill.go
+++ bill.go
@@ -29,7 +29,17 @@ func (b bill) format() string {
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-25v ....$%0.2f", "total :", total)
+	fs += fmt.Sprintf("%-25v...$%0.2f", "total", b.tip)
+
+	fs += fmt.Sprintf("%-25v ....$%0.2f", "total :", total+b.tip)
 
 	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func createill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("create a new bill: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	b := newBill(name)
+
+	fmt.Println("created a bill - ", b.name)
+
+	return b
+}
 
 func main() {
-	mybill := newBill("mario's bill")
+	myBill := createill()
 
-	fmt.Println(mybill.format())
+	fmt.Println(myBill)
+
 }
+
+// func main() {
+// 	mybill := newBill("mario's bill")
+// 	mybill.addItem("veger", 10.3)
+// 	mybill.addItem("veerg", 70.3)
+// 	mybill.addItem("vrg", 15.3)
+// 	mybill.updateTip(10)
+// 	fmt.Println(mybill.format())
+// }
