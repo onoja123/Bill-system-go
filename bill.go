2489e2c6fba9323612e8f72a59ac5a0034ae3664
okpe onoja <okpeonoja18@gmail.com>
2023-03-15 01:20:56 +0100

    done

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -18,28 +21,43 @@ func newBill(name string) bill {
 	return b
 }
 
-func (b bill) format() string {
-	fs := "Bill breakdown: \n"
+// add item to bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
 
+// format the bill
+func (b *bill) format() string {
+	fs := "Bill breakdown:\n"
 	var total float64 = 0
 
+	// list items
 	for k, v := range b.items {
-		fs += fmt.Sprintf("%-25v...$%v \n", k+": ", v)
-
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-25v...$%0.2f", "total", b.tip)
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
 
-	fs += fmt.Sprintf("%-25v ....$%0.2f", "total :", total+b.tip)
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
 }
 
+// update tip
 func (b *bill) updateTip(tip float64) {
-	b.tip = tip
+	(*b).tip = tip
+	// b.tip = tip
 }
 
-func (b *bill) addItem(name string, price float64) {
-	b.items[name] = price
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill saved to file")
 }
--- main.go
+++ main.go
@@ -4,35 +4,71 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func createill() bill {
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("create a new bill: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
-
-	fmt.Println("created a bill - ", b.name)
+	fmt.Println("Created the bill -", b.name)
 
 	return b
 }
 
-func main() {
-	myBill := createill()
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println(myBill)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
 
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number...")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println("item added -", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number...")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("tip has been updated to", tip)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("bill has been saved as", b.name)
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
 }
 
-// func main() {
-// 	mybill := newBill("mario's bill")
-// 	mybill.addItem("veger", 10.3)
-// 	mybill.addItem("veerg", 70.3)
-// 	mybill.addItem("vrg", 15.3)
-// 	mybill.updateTip(10)
-// 	fmt.Println(mybill.format())
-// }
+func main() {
+
+	mybill := createBill()
+	promptOptions(mybill)
+
+}
